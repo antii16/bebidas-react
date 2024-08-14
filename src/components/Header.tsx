7e5776fa24ffe48806f5979c3f2d95d10054d4cc
Antonella <98277771+antii16@users.noreply.github.com>
2024-08-14 20:53:00 +0200

    Theming en Tailwind, múltiples stores con zustand y obtener categorías de API y validar con ZOD

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,45 @@
+import { useEffect, useMemo, useState, ChangeEvent, FormEvent } from "react"
 import { NavLink, useLocation } from "react-router-dom"
+import { useAppStore } from "../stores/useAppStore"
 
 export default function Header() {
+    const[searchFilters, setSearchFilters] = useState({
+        ingredient: '',
+        category: ''
+    })
 
-    const {pathname} = useLocation()
+    const { pathname } = useLocation()
+    const isHome = useMemo(() => pathname === '/', [pathname])
 
+    const fetchCategories = useAppStore((state) => state.fetchCategories)
+    const categories = useAppStore((state) => state.categories)
+    const searchRecipes = useAppStore((state) => state.searchRecipes)
+    
+    useEffect(() => {
+        fetchCategories()
+    }, [])
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement> ) => {
+        setSearchFilters({
+            ...searchFilters,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        //TODO: validar 
+        if(Object.values(searchFilters).includes('')){
+            console.log('Todos los campos son obligatorios')
+            return
+        }
+
+        // Consultar las recetas
+        searchRecipes(searchFilters)
+    }
     return (
-        <header className="bg-slate-800">
+        <header className={isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800'}>
             <div className="mx-auto container px-5 py-16">
                 <div className="flex justify-between items-center">
                     <div>
@@ -14,18 +48,64 @@ export default function Header() {
                     <nav className="flex gap-4">
                         <NavLink
                             to="/"
-                            className={({isActive}) => 
-                                isActive ? 'text-orange-500 uppercase font-bold' :'text-white uppercase font-bold'
+                            className={({ isActive }) =>
+                                isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
                             }
                         >Inicio</NavLink>
                         <NavLink
                             to="/favoritos"
-                            className={({isActive}) => 
-                                isActive ? 'text-orange-500 uppercase font-bold' :'text-white uppercase font-bold'
+                            className={({ isActive }) =>
+                                isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
                             }
                         >Favoritos</NavLink>
                     </nav>
                 </div>
+                {isHome && (
+                    <form
+                        className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+                        onSubmit={handleSubmit}
+                        >
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Nombre o Ingredientes</label>
+                            <input type="text"
+                                id="ingredient"
+                                name="ingredient"
+                                className="p-3 w-full rounded-lg focus:outline-none"
+                                placeholder="Nombre o Ingrediente. Ej. Vodka, Café, Tequila"
+                                onChange={handleChange}
+                                value={searchFilters.ingredient}
+                            />
+                        </div>
+
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="category"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Categoría</label>
+                            <select
+                                id="category"
+                                name="category"
+                                className="p-3 w-full rounded-lg focus:outline-none"
+                                onChange={handleChange}
+                                value={searchFilters.category}
+                            >
+                                <option value="">-- Seleccione --</option>
+                                {categories.drinks.map(category => (
+                                    <option
+                                        value={category.strCategory}
+                                        key={category.strCategory}
+                                    >{category.strCategory}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <input type="submit" value='Buscar'
+                            className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+                        />
+                    </form>
+                )}
             </div>
         </header>
     )
---
+++ src/stores/recipeSlice.ts
@@ -0,0 +1,25 @@
+import { StateCreator } from "zustand"
+import { getCategories } from "../services/RecipeService"
+import { Categories, SearchFilter } from "../types"
+
+
+export type RecipeSliceType = {
+    categories: Categories
+    fetchCategories: () => Promise<void>
+    searchRecipes: (searchFilter: SearchFilter) => Promise<void>
+}
+
+export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) =>({
+    categories: {
+        drinks: []
+    },
+    fetchCategories: async ()=> {
+       const categories = await getCategories()
+        set({
+           categories 
+        })
+    },
+    searchRecipes: async (filters)=> {
+        console.log(filters)
+    }
+})
